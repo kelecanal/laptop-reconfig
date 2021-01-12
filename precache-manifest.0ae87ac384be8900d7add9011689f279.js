10c1e502d548d9399750f668bf5b78bed5a1f258
Kareine Elecanal <kaelecanal@gmail.com>
2021-01-11 22:56:12 -0800

    Updates

---
+++ precache-manifest.0ae87ac384be8900d7add9011689f279.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c233d9be267e5e7dbff9806b0b18632",
+    "url": "/laptop-reconfig/index.html"
+  },
+  {
+    "revision": "774b56a5a749996c97a9",
+    "url": "/laptop-reconfig/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "91be17f00bf027269ee1",
+    "url": "/laptop-reconfig/static/js/2.a3f18fbf.chunk.js"
+  },
+  {
+    "revision": "774b56a5a749996c97a9",
+    "url": "/laptop-reconfig/static/js/main.160ca73c.chunk.js"
+  },
+  {
+    "revision": "81a36961f6d9b6c77181",
+    "url": "/laptop-reconfig/static/js/runtime~main.59c0cf3a.js"
+  }
+]);
